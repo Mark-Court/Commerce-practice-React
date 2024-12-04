ca92fc8e72a78b2f38fe4d6b9ef04e4f96abaea4
Mark <courtademark@gmail.com>
2024-12-03 18:25:54 -0700

    Books Inprogress

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import Nav from "./components/Nav.jsx";
+import Footer from "./components/Footer.jsx";
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min.js";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact component={Books} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
